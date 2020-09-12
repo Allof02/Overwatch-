ffa5fd67dcf7afc591f2eb5b5f123f69d8961ce7
Pete Chen <petecjh@gmail.com>
2020-09-12 10:42:03 -0700

    more

--- web demo.py
+++ web demo.py
@@ -5,6 +5,9 @@ r = requests.get('https://owapi.io/profile/pc/us/ZEROTWO-12301')
 print(r.json())
 data = r.json()
 
+#Variables
+
+private = data['private']
 
 username = data["username"]
 level = data["level"]
@@ -33,7 +36,17 @@ dpsrank_img = data['competitive']['damage']['rank_img']
 suprank = data['competitive']['support']['rank']
 suprank_img = data['competitive']['support']['rank_img']
 
+#Tring to do a loop, for example, if private is true, then stop importing csv and print 'private', if private is false, then continue to import csv
+#but dont know how =.=
+
+#if private == 'false'
+   # print('Private')
+        #break
+#else:
+
 import csv
+
+#CSV WRITERS 
 
 with open ('xxx.csv' , newline='') as csv_file:
     csv_reader = csv.reader(csv_file, delimiter = ',')
@@ -45,7 +58,31 @@ with open('xxx.csv' , 'w', newline='') as csv_file:
     csv_writer.writerow(['Username:'] + [username])
     csv_writer.writerow(['Level:'] + [level])
     csv_writer.writerow(['Portrait:'] + [portrait])
-    csv_writer.writerow(['Playtime' + '(Quickplay):'] + [qptime])
+    csv_writer.writerow(['---QuickPlay---'])
+    csv_writer.writerow(['Playtime:'] + [qptime])
+    csv_writer.writerow(['Won:'] + [qpwon])
+    csv_writer.writerow(['Played:'] + [qptotal])
+    csv_writer.writerow(['---Competitive---'])
+    csv_writer.writerow(['Playedtime:'] + [comptime])
+    csv_writer.writerow(['Won:'] + [compwon])
+    csv_writer.writerow(['Lost:'] + [complose])
+    csv_writer.writerow(['Draw:'] + [compdraw])
+    csv_writer.writerow(['Played:'] + [comptotal])
+    csv_writer.writerow(['Win' , 'Rate:'] + [compwinrate])
+    csv_writer.writerow(['---Ranking---'])
+    csv_writer.writerow(['[Tank]'])
+    csv_writer.writerow(['Rank:'] + [tankrank])
+    csv_writer.writerow(['IMG:'] + [tankrank_img])
+    csv_writer.writerow(['[Damage]'])
+    csv_writer.writerow(['Rank:'] + [dpsrank])
+    csv_writer.writerow(['IMG:'] + [dpsrank_img])
+    csv_writer.writerow(['[Support]'])
+    csv_writer.writerow(['Rank:'] + [suprank])
+    csv_writer.writerow(['IMG:'] + [suprank_img])
+
+
+    
+    
 
 
 
