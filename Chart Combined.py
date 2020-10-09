1702c075ae897be0a167ce1a844f4f7881f4cf89
Pete Chen <petecjh@gmail.com>
2020-10-08 22:25:32 -0700

    ~~

--- Chart Combined.py
+++ Chart Combined.py
@@ -136,3 +136,50 @@ fig.add_trace(
 )
 fig.update_layout(legend_title_text='Sigma')
 fig.show()
+
+
+import plotly.graph_objects as go
+from plotly.subplots import make_subplots
+import pandas as pd 
+
+df = pd.read_csv('herodata.csv', ' ')
+
+fig = make_subplots(rows=2, cols=2,
+                    specs=[[{"secondary_y": True}, {"secondary_y": True}],
+                           [{"secondary_y": True}, {"secondary_y": True}]]
+                           )
+
+
+fig.add_trace(
+    go.Scatter(x = df['current_Date'], y= df['orisa_elimperlife'], name="Eliminations Per Life"),
+    row=1, col=1, secondary_y=False
+)
+
+fig.add_trace(
+    go.Scatter(x= df['current_Date'], y=df['orisa_weaponAccu'], name="Weapon Accuracy"),
+    row=1, col=1, secondary_y=True,
+)
+
+
+fig.add_trace(
+    go.Scatter(x=df['current_Date'], y=df['orisa_gamewon'], name="Games Won"),
+    row=1, col=2, secondary_y=False,
+)
+
+fig.add_trace(
+    go.Scatter(x= df['current_Date'], y= df['orisa_multikills'], name="MultiKills Best"),
+    row=1, col=2, secondary_y=True,
+)
+
+
+fig.add_trace(
+    go.Scatter(x=df['current_Date'], y=df['orisa_objkill'], name="Objective Kills"),
+    row=2, col=1, secondary_y=False,
+)
+
+fig.add_trace(
+    go.Scatter(x=df['current_Date'], y=df['orisa_winper'], name="Win Percentage"),
+    row=2, col=1, secondary_y=True,
+)
+fig.update_layout(legend_title_text='Orisa')
+fig.show()
--- herodata.py
+++ herodata.py
@@ -4,44 +4,44 @@ print(r.json())
 data = r.json()
 
 def get_orisa_data(data):
-    orisa_gamewon = data['quickPlayStats']['topHeroes']['orisa']['gamesWon']
-    orisa_weaponAccu = data['quickPlayStats']['topHeroes']['orisa']['weaponAccuracy']
+    orisa_gamewon = data['quickPlayStats']['careerStats']['orisa']['game']['gamesWon']
+    orisa_weaponAccu = data['quickPlayStats']['careerStats']['orisa']['combat']['weaponAccuracy']
     orisa_multikills = data['quickPlayStats']['topHeroes']['orisa']["multiKillBest"] 
-    orisa_objkill = data['quickPlayStats']['topHeroes']['orisa']["objectiveKills"] 
-    orisa_winper = data['quickPlayStats']['topHeroes']['orisa']["winPercentage"] 
-    orisa_elimperlife = data['quickPlayStats']['topHeroes']['orisa']['eliminationsPerLife']
+    orisa_objkill = data['quickPlayStats']['careerStats']['orisa']['combat']["objectiveKills"] 
+    orisa_winper = data['quickPlayStats']['careerStats']['orisa']['game']["winPercentage"] 
+    orisa_elimperlife = data['quickPlayStats']['careerStats']['orisa']['average']['eliminationsPerLife']
 
     return [orisa_gamewon, orisa_weaponAccu, orisa_multikills, orisa_objkill, orisa_winper, orisa_elimperlife]
 
 def get_dva_data(data):
-    dva_gamewon = data['quickPlayStats']['topHeroes']["dVa"]["gamesWon"]
-    dva_weaponAccu = data['quickPlayStats']['topHeroes']["dVa"]["weaponAccuracy"]
-    dva_multikills = data['quickPlayStats']['topHeroes']["dVa"]["multiKillBest"]
-    dva_objkill = data['quickPlayStats']['topHeroes']["dVa"]["objectiveKills"]
-    dva_winper = data['quickPlayStats']['topHeroes']["dVa"]["winPercentage"]
-    dva_elimperlife = data['quickPlayStats']['topHeroes']["dVa"]["eliminationsPerLife"]
+    dva_gamewon = data['quickPlayStats']['careerStats']["dVa"]['game']["gamesWon"]
+    dva_weaponAccu = data['quickPlayStats']['careerStats']["dVa"]['combat']["weaponAccuracy"]
+    dva_multikills = data['quickPlayStats']['careerStats']["dVa"]['best']["multikillsBest"]
+    dva_objkill = data['quickPlayStats']['careerStats']["dVa"]['combat']["objectiveKills"]
+    dva_winper = data['quickPlayStats']['careerStats']["dVa"]['game']["winPercentage"]
+    dva_elimperlife = data['quickPlayStats']['careerStats']["dVa"]['average']["eliminationsPerLife"]
 
     return [dva_gamewon, dva_weaponAccu, dva_multikills, dva_objkill, dva_winper, dva_elimperlife]
 
 
 
 def get_zarya_data(data):
-    zarya_elimperlife = data['quickPlayStats']['topHeroes']['zarya']['eliminationsPerLife']
-    zarya_weaponAcc = data['quickPlayStats']['topHeroes']['zarya']['weaponAccuracy']
-    zarya_gamewon = data['quickPlayStats']['topHeroes']['zarya']["gamesWon"] 
-    zarya_multikill = data['quickPlayStats']['topHeroes']['zarya']["multiKillBest"] 
-    zarya_objkill = data['quickPlayStats']['topHeroes']['zarya']["objectiveKills"] 
-    zarya_winper = data['quickPlayStats']['topHeroes']['zarya']["winPercentage"] 
+    zarya_elimperlife = data['quickPlayStats']['careerStats']['zarya']['average']['eliminationsPerLife']
+    zarya_weaponAcc = data['quickPlayStats']['careerStats']['zarya']['combat']['weaponAccuracy']
+    zarya_gamewon = data['quickPlayStats']['careerStats']['zarya']['game']["gamesWon"] 
+    zarya_multikill = data['quickPlayStats']['careerStats']['zarya']['best']["multikillsBest"] 
+    zarya_objkill = data['quickPlayStats']['careerStats']['zarya']['combat']["objectiveKills"] 
+    zarya_winper = data['quickPlayStats']['careerStats']['zarya']['game']["winPercentage"] 
     
     return [zarya_elimperlife, zarya_weaponAcc, zarya_gamewon, zarya_multikill, zarya_objkill, zarya_winper]
 
 def get_sigma_data(data):
-    sig_gamewon = data['quickPlayStats']['topHeroes']['sigma']['gamesWon']
+    sig_gamewon = data['quickPlayStats']['careerStats']['sigma']['game']['gamesWon']
     sig_weaponAccu = data['quickPlayStats']['topHeroes']['sigma']['weaponAccuracy']
-    sig_multikills = data['quickPlayStats']['topHeroes']['sigma']['multiKillBest']
-    sig_objkill = data['quickPlayStats']['topHeroes']['sigma']['objectiveKills']
-    sig_winper = data['quickPlayStats']['topHeroes']['sigma']['winPercentage']
-    sig_elimperlife = data['quickPlayStats']['topHeroes']['sigma']['eliminationsPerLife']
+    sig_multikills = data['quickPlayStats']['careerStats']['sigma']['best']['multikillsBest']
+    sig_objkill = data['quickPlayStats']['careerStats']['sigma']['combat']['objectiveKills']
+    sig_winper = data['quickPlayStats']['careerStats']['sigma']['game']['winPercentage']
+    sig_elimperlife = data['quickPlayStats']['careerStats']['sigma']['average']['eliminationsPerLife']
 
     return [sig_gamewon, sig_weaponAccu, sig_multikills, sig_objkill, sig_winper, sig_elimperlife]
 
