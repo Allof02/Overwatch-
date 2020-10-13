9ef28113a1d4b55d5e7818a6275911e61e6a1350
ktongs <kingsley@eneris.ca>
2020-10-12 17:26:53 -0700

    added comments for review, DO NOT MERGE THIS BRANCH

--- PlayersProfile.py
+++ PlayersProfile.py
@@ -1,5 +1,42 @@
+# try to have all your imports at the beginning of each file
+
+# install "Trailing spaces" extension in VS code, this will highlight all the extra white spaces at the end of the file and you can remove them
 
 import requests
+
+# if you're using an api, it's common to have some sample in the code file to explain the expected behaviour.
+'''
+    https://ow-api.com/v1/stats/pc/us/Z9HONG-1578/heroes/roadhog      API for specific heroes
+    https://ow-api.com/v1/stats/pc/us/Z9HONG-1578/complete  MORE INFO
+
+    Example output:
+
+        Username: Z9HONG
+        Level: 57
+        Portrait: https://d15f34w2p8l1cc.cloudfront.net/overwatch/1d754ab4338bb097f0bb7d69fe4f14c41599bc5b3ea5fa21fef68a5a1b4f9796.png
+        ---QuickPlay---
+        Playtime: 47:05:46
+        Won: 181
+        ---Competitive---
+        Playedtime: 48:49
+        Won: 3
+        Lost: 2
+        Draw: 0
+        Played: 5
+        Win Rate: 
+        ---Ranking---
+        [Tank]
+        Rank: 2631
+        IMG: https://d1u1mce87gyfbn.cloudfront.net/game/rank-icons/rank-PlatinumTier.png
+        [Damage]
+        Rank: 
+        IMG: 
+        [Support]
+        Rank: 
+        IMG: 
+        CurrentTime: 13:04:33
+'''
+
 
 r = requests.get('https://owapi.io/profile/pc/us/ZEROTWO-12301')
 print(r.json())
@@ -16,6 +53,7 @@ portrait = data["portrait"]
 
 quickplay = data["games"]['quickplay']
 qpwon = data['games']['quickplay']['won']
+# remove comment
 #qptotal = data['games']['quickplay']['played']       !!!Some players' profile doesn't show total played games for Quickplay!!! 下周一对此处修改
 qptime = data['playtime']['quickplay']
 
@@ -36,6 +74,8 @@ dpsrank_img = data['competitive']['damage']['rank_img']
 suprank = data['competitive']['support']['rank']
 suprank_img = data['competitive']['support']['rank_img']
 
+# try to have all your imports at the beginning of each file
+
 import time
 
 t = time.localtime()
@@ -45,10 +85,13 @@ print(current_time)
 #do a loop, for example, if private is true, then stop importing csv and print 'private', if private is false, then continue to import csv
 #
 
+# Remove testing code, only leave in comments to explain things
 #if private == 'false'
    # print('Private')
         #break
 #else:
+
+# try to have all your imports at the beginning of each file
 
 import csv
 
@@ -66,6 +109,7 @@ with open('xxx.csv' , 'w', newline='') as csv_file:
     csv_writer.writerow(['---QuickPlay---'])
     csv_writer.writerow(['Playtime:'] + [qptime])
     csv_writer.writerow(['Won:'] + [qpwon])
+    # remove comments
     #csv_writer.writerow(['Played:'] + [qptotal])          
     csv_writer.writerow(['---Competitive---'])
     csv_writer.writerow(['Playedtime:'] + [comptime])
@@ -89,6 +133,7 @@ with open('xxx.csv' , 'w', newline='') as csv_file:
 
 
 
+#  maybe move this to top above r = requests.get(...) line.
 
    ### https://ow-api.com/v1/stats/pc/us/Z9HONG-1578/heroes/roadhog      API for specific heroes ###
    ### https://ow-api.com/v1/stats/pc/us/Z9HONG-1578/complete  MORE INFO ###
